4b50cfff55cf2b87be1269282539be141787a748
panyaxbo <panya.javamania@gmail.com>
2021-08-21 22:35:47 +0700

    add function check string and convert struct to string

--- utilx/go.mod
+++ utilx/go.mod
@@ -1,3 +1,5 @@
 module github.com/panyaxbo/libs/utilx
 
 go 1.16
+
+require github.com/panyaxbo/libs/logx v0.0.29
--- utilx/utilx.go
+++ utilx/utilx.go
@@ -1,6 +1,11 @@
 package utilx
 
-import "strings"
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/panyaxbo/libs/logx"
+)
 
 const (
 	escapeChar = "\b\f\v\r\t\n"
@@ -8,4 +13,17 @@ const (
 
 func RemoveEscapeCharactor(str string) string {
 	return strings.TrimSuffix(strings.TrimRight(strings.TrimSpace(str), escapeChar), escapeChar)
+}
+
+func StructToString(data interface{}) string {
+	d, err := json.Marshal(data)
+	if err != nil {
+		logx.Errorf("Cannot Convert Struct -> String %s", err)
+		return ""
+	}
+	return string(d)
+}
+
+func IsStringNullBlank(str string) bool {
+	return str == "" || len(str) <= 0
 }
