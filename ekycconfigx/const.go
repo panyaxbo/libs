36c0b525091c78e06a975c49b70ccb6ac1f1b21f
panyaxbo <panya.javamania@gmail.com>
2022-01-17 16:01:12 +0700

    change redis host on prod

--- ekycconfigx/const.go
+++ ekycconfigx/const.go
@@ -15,6 +15,6 @@ var (
 		redisHostLocal: "127.0.0.1:6379",
 		redisHostDev:   "10.98.3.99:6379",
 		redisHostUat:   "10.98.3.91:6379",
-		redisHostProd:  "10.99.1.140:6379",
+		redisHostProd:  "10.99.1.52:6379",
 	}
 )
