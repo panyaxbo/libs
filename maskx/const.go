f182d33b1deb8b0f312647aab7dea7352d458afa
panyaxbo <panya.javamania@gmail.com>
2022-08-09 22:22:05 +0700

    update masks with symbol

---
+++ maskx/const.go
@@ -0,0 +1,7 @@
+package maskx
+
+const (
+	SymbolStar  = "*"
+	SymbolDash  = "-"
+	SymbolSharp = "#"
+)
--- maskx/maskx.go
+++ maskx/maskx.go
@@ -4,14 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
-	"runtime"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/panyaxbo/libs/logx"
-	"github.com/spf13/viper"
+	"unicode/utf8"
 )
 
 var defaultSensitiveData = []string{
@@ -50,7 +47,9 @@ var defaultSensitiveData = []string{
 // )
 
 type Mask interface {
-	Json(b []byte, env string) (*string, error)
+	Json(b []byte) (*string, error)
+	JsonMaskEncrypted(b []byte, env string) (*string, error)
+	JsonMaskSymbol(b []byte, symbol string) (*string, error)
 }
 
 type mask struct {
@@ -62,7 +61,6 @@ type mask struct {
 func Init(fields ...[]string) Mask {
 	var f = defaultSensitiveData
 
-	initConfig()
 	if len(fields) > 0 {
 		f = append(f, fields[0]...)
 	}
@@ -75,22 +73,22 @@ func Init(fields ...[]string) Mask {
 	}
 }
 
-func initConfig() {
-	viper.AddConfigPath(".")
+// func initConfig() {
+// 	viper.AddConfigPath(".")
 
-	if err := viper.ReadInConfig(); err != nil {
-		logx.Panic(err)
-	}
+// 	if err := viper.ReadInConfig(); err != nil {
+// 		logx.Panic(err)
+// 	}
 
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+// 	viper.AutomaticEnv()
+// 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	runtime.GOMAXPROCS(1)
+// 	runtime.GOMAXPROCS(1)
 
-	logx.Init(viper.GetString("log.level"), viper.GetString("log.env"))
-}
+// 	logx.Init(viper.GetString("log.level"), viper.GetString("log.env"))
+// }
 
-func (m mask) Json(b []byte, env string) (*string, error) {
+func (m mask) Json(b []byte) (*string, error) {
 	var storage []interface{}
 	p := make(chan bool, 10)
 	var wg sync.WaitGroup
@@ -101,7 +99,33 @@ func (m mask) Json(b []byte, env string) (*string, error) {
 		return nil, err
 	}
 	wg.Wait()
-	return m.masking(b, storage, env)
+	return m.masking(b, storage)
+}
+func (m mask) JsonMaskEncrypted(b []byte, env string) (*string, error) {
+	var storage []interface{}
+	p := make(chan bool, 10)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	p <- true
+	err := m.walkThrough(b, &storage, p, &wg)
+	if err != nil {
+		return nil, err
+	}
+	wg.Wait()
+	return m.maskingWithEncrypted(b, storage, env)
+}
+func (m mask) JsonMaskSymbol(b []byte, symbol string) (*string, error) {
+	var storage []interface{}
+	p := make(chan bool, 10)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	p <- true
+	err := m.walkThrough(b, &storage, p, &wg)
+	if err != nil {
+		return nil, err
+	}
+	wg.Wait()
+	return m.maskingWithSymbol(b, storage, symbol)
 }
 
 //walkThrough will recursive until no more array or map
@@ -162,52 +186,86 @@ func (m mask) sensitive(k string, v interface{}, storage *[]interface{}) {
 	}
 }
 
-func (m mask) masking(j []byte, d []interface{}, env string) (*string, error) {
+func (m mask) masking(j []byte, d []interface{}) (*string, error) {
+	body := string(j)
+	if len(d) == 0 {
+		return &body, nil
+	}
+	for _, val := range d {
+
+		body = strings.ReplaceAll(body, typeCasting(val.(interface{})), randomMask(typeCasting(val.(interface{}))))
+	}
+	return &body, nil
+}
+
+func (m mask) maskingWithEncrypted(j []byte, d []interface{}, env string) (*string, error) {
 	body := string(j)
 	if len(d) == 0 {
 		return &body, nil
 	}
 	for _, val := range d {
 		if strings.EqualFold(env, "prd") || strings.EqualFold(env, "prod") || strings.EqualFold(env, "production") {
-			body = strings.ReplaceAll(body, typeCasting(val.(interface{})), randomMask(m.gcmPrd, typeCasting(val.(interface{}))))
+			body = strings.ReplaceAll(body, typeCasting(val.(interface{})), randomMaskEncrypted(m.gcmPrd, typeCasting(val.(interface{}))))
 		} else {
-			body = strings.ReplaceAll(body, typeCasting(val.(interface{})), randomMask(m.gcmUat, typeCasting(val.(interface{}))))
+			body = strings.ReplaceAll(body, typeCasting(val.(interface{})), randomMaskEncrypted(m.gcmUat, typeCasting(val.(interface{}))))
 		}
 	}
 	return &body, nil
 }
 
-func randomMask(gcm *AES, c string) string {
+func (m mask) maskingWithSymbol(j []byte, d []interface{}, symbol string) (*string, error) {
+	body := string(j)
+	if len(d) == 0 {
+		return &body, nil
+	}
+	for _, val := range d {
+		body = strings.ReplaceAll(body, typeCasting(val.(interface{})), randomMaskSymbol(symbol, typeCasting(val.(interface{}))))
+	}
+	return &body, nil
+}
+func randomMask(c string) string {
 	if len(c) == 0 {
 		return c
 	}
-	// var r = []rune(c)
-	// var cl = len(r)
-	// var size = initMaskSize(cl)
-	// var count int
-	// raffle := make(map[int]int, size)
-	// for i := 0; i < cl; i++ {
-	// 	count += 1 //avoid random forever
-	// 	if len(raffle) == size || count == 10 {
-	// 		//break if mask enough
-	// 		break
-	// 	}
-	// 	v := randPos(cl)
-	// 	if _, ok := raffle[v]; ok {
-	// 		i -= 1
-	// 		continue
-	// 	}
-	// 	//case not mask yet
-	// 	if len(r)-1 >= v {
-	// 		r[v] = '*'
-	// 		raffle[v] = v
-	// 	}
-	// }
-	// return string(r)
+	var r = []rune(c)
+	var cl = len(r)
+	var size = initMaskSize(cl)
+	var count int
+	raffle := make(map[int]int, size)
+	for i := 0; i < cl; i++ {
+		count += 1 //avoid random forever
+		if len(raffle) == size || count == 10 {
+			//break if mask enough
+			break
+		}
+		v := randPos(cl)
+		if _, ok := raffle[v]; ok {
+			i -= 1
+			continue
+		}
+		//case not mask yet
+		if len(r)-1 >= v {
+			r[v] = '*'
+			raffle[v] = v
+		}
+	}
+	return string(r)
+}
+func randomMaskEncrypted(gcm *AES, c string) string {
+	if len(c) == 0 {
+		return c
+	}
 	return gcm.Encrypt(c)
 
 }
+func randomMaskSymbol(symbol string, c string) string {
+	if len(c) == 0 {
+		return c
+	}
 
+	return strings.Repeat(symbol, utf8.RuneCountInString(c))
+
+}
 func randPos(max int) int {
 	source := rand.NewSource(time.Now().UnixNano())
 	ra := rand.New(source)
