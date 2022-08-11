cb5e5bacdae0f26bece9a35647507871d4d5da0a
panyaxbo <panya.javamania@gmail.com>
2022-08-11 09:48:54 +0700

    update field nname

--- configx/configx.go
+++ configx/configx.go
@@ -1,18 +1,18 @@
 package configx
 
 type ConfigMaskLog struct {
-	withMaskLogWithEncrypt bool
-	withMaskLogWithSymbol  bool
-	Env                    string
-	Symbol                 string
+	IsMaskLogWithEncrypt bool
+	IsMaskLogWithSymbol  bool
+	Env                  string
+	Symbol               string
 }
 
 func (c *ConfigMaskLog) WithMaskingLogWithEncrypted(env string) {
-	c.withMaskLogWithEncrypt = true
+	c.IsMaskLogWithEncrypt = true
 	c.Env = env
 }
 
 func (c *ConfigMaskLog) WithMaskingLogWithSymbol(symbol string) {
-	c.withMaskLogWithSymbol = true
+	c.IsMaskLogWithSymbol = true
 	c.Symbol = symbol
 }
