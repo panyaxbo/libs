7a8b0ca149b11780b9d096d6b87dc8934908c042
panyaxbo <panya.javamania@gmail.com>
2022-08-15 13:07:30 +0700

    check len request string

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -165,7 +165,7 @@ func LoggerWithMaskLog(config *configx.ConfigMaskLog) echo.MiddlewareFunc {
 			//	maskTool := NewMaskTool(filter.FieldFilter("identifier"))
 
 			if config.IsMaskLogWithEncrypt {
-				if req.Body != nil || string(b) != "" {
+				if string(b) != "" || len(string(b)) <= 0 {
 					m := maskx.Init(configx.SensitiveFields)
 					t, err := m.JsonMaskEncrypted(b, config.Env)
 					if err != nil {
@@ -182,7 +182,7 @@ func LoggerWithMaskLog(config *configx.ConfigMaskLog) echo.MiddlewareFunc {
 					}).Info("echo request information")
 				}
 			} else if config.IsMaskLogWithSymbol {
-				if req.Body != nil || string(b) != "" {
+				if string(b) != "" || len(string(b)) <= 0 {
 					m := maskx.Init(configx.SensitiveFields)
 					t, err := m.JsonMaskSymbol(b, config.Symbol)
 					if err != nil {
