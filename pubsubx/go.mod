a2b060d1eb3b5f59d564f8b4271bc097b2430ceb
panyaxbo <panya.javamania@gmail.com>
2022-09-22 13:32:27 +0700

    update pubsub

--- pubsubx/go.mod
+++ pubsubx/go.mod
@@ -4,8 +4,11 @@ go 1.16
 
 require (
 	cloud.google.com/go/pubsub v1.12.2
-	github.com/panyaxbo/libs/logx v0.0.29
+	github.com/panyaxbo/libs/configx v0.0.5
+	github.com/panyaxbo/libs/logx v0.0.30
+	github.com/panyaxbo/libs/maskx v0.0.9
+	github.com/panyaxbo/libs/utilx v0.1.2
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.8.1
+	github.com/sirupsen/logrus v1.9.0
 	google.golang.org/api v0.50.0
 )
--- pubsubx/pub.go
+++ pubsubx/pub.go
@@ -6,7 +6,10 @@ import (
 	"time"
 
 	"cloud.google.com/go/pubsub"
+	"github.com/panyaxbo/libs/configx"
 	"github.com/panyaxbo/libs/logx"
+	"github.com/panyaxbo/libs/maskx"
+	"github.com/panyaxbo/libs/utilx"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -21,6 +24,7 @@ type Pub struct {
 	Client    *pubsub.Client
 	Timeout   time.Duration
 	ProjectID string
+	Config    *configx.ConfigMaskLog
 }
 
 func NewPub(cfg PubConfig) *Pub {
@@ -32,6 +36,19 @@ func NewPub(cfg PubConfig) *Pub {
 		Client:    cfg.Client,
 		Timeout:   cfg.Timeout,
 		ProjectID: cfg.ProjectID,
+	}
+}
+
+func NewPubWithMaskLog(cfg PubConfig, config *configx.ConfigMaskLog) *Pub {
+	if cfg.Client == nil || cfg.Timeout == 0 || cfg.ProjectID == "" {
+		logx.Panic("pubsub client, timeout, topicID, projectID required")
+	}
+
+	return &Pub{
+		Client:    cfg.Client,
+		Timeout:   cfg.Timeout,
+		ProjectID: cfg.ProjectID,
+		Config:    config,
 	}
 }
 
@@ -58,4 +75,30 @@ func (p *Pub) Publish(ctx context.Context, topicID string, i interface{}) error 
 	}).Info("pub information")
 
 	return errors.WithStack(err)
+}
+
+func (p *Pub) logValue(b []byte) string {
+	if p.Config == nil {
+		return logx.LimitMSGByte(b)
+	}
+
+	m := maskx.Init(configx.SensitiveFields)
+
+	if p.Config.IsMaskLogWithEncrypt {
+		s, err := m.JsonMaskEncrypted(b, p.Config.Env)
+		if err != nil {
+			panic(err)
+		}
+		return logx.LimitMSGByte(utilx.StringPointerToByteArray(s))
+	}
+
+	if p.Config.IsMaskLogWithSymbol {
+		s, err := m.JsonMaskSymbol(b, p.Config.Symbol)
+		if err != nil {
+			panic(err)
+		}
+		return logx.LimitMSGByte(utilx.StringPointerToByteArray(s))
+	}
+
+	return logx.LimitMSGByte(b)
 }
